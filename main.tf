a52c66d94d35b2b5f0b4c49e902ce99a0589fb93
dvation <9741231+Dvation@users.noreply.github.com>
2023-09-24 12:50:32 -0400

    #9 Install terraform provider, Create s3 bucket (#10)

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
     }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,11 +20,17 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  length  = 32
+  special = false
+  lower   = true
+  upper   = false
+  numeric = false
 }
 
 output "random_bucket_string" {
-    value = random_string.bucket_name.result
+  value = random_string.bucket_name.result
 }
 
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+}
