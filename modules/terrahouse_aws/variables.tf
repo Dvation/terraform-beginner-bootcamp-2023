99a0c46d3ca6cc5d0840efcef599ed25dd82bcb0
Tony B <Dvation@gmail.com>
2023-09-30 15:08:48 +0000

    #24 enabled static website hosting, uploaded index and error file via terraform

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -9,3 +9,24 @@ variable "user_uuid" {
 }
 
 variable "bucket_name" {}
+
+variable "index_html_filepath" {
+  description = "The file path for the index.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The specified index_html_filepath does not exist."
+  }
+}
+
+
+variable "error_html_filepath" {
+  description = "The file path for the error.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The specified error_html_filepath does not exist."
+  }
+}
